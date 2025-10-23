f32e4e66bccc91e59854a376b014e3fa39a107f1
lnstw <ojhggguuim@gmail.com>
2025-10-23 21:15:48 +0800

    fix: channel.py 修正

--- service/channel.py
+++ service/channel.py
@@ -14,19 +14,3 @@ async def send_message_to_last_channel(client: MusicClient, guild_id: int, messa
                         await channel.send(message, silent=True)
                 except Exception as e:
                     print(f"發送訊息時發生錯誤: {e}")
-import discord
-from core import MusicClient
-
-async def send_message_to_last_channel(client: MusicClient, guild_id: int, message: str = None, embed: discord.Embed = None):
-    if guild_id in client.last_channels:
-        guild = client.get_guild(guild_id)
-        if guild:
-            channel = guild.get_channel(client.last_channels[guild_id])
-            if channel:
-                try:
-                    if embed:
-                        await channel.send(embed=embed, silent=True)
-                    elif message:
-                        await channel.send(message, silent=True)
-                except Exception as e:
-                    print(f"發送訊息時發生錯誤: {e}")
